734260bfd771da84ff9951ac3f7215936cdb9c3d
Martin Zhang <jianwu.zhang@rakuten.com>
2025-04-21 18:10:29 +0800

    Updated bcrypt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import './global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`page-root-layout ${inter.className} antialiased`}>{children}</body>
+      {/* <body>{children}</body> */}
     </html>
   );
 }
